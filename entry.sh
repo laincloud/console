3fff393695aeb26e55eb07ed4579e1dfa5ea00c0
lipan <panli7@creditease.cn>
2016-05-11 14:58:49 +0800

    modify some init msg

--- entry.sh
+++ entry.sh
@@ -3,20 +3,19 @@
 environ=$LAIN_DOMAIN
 clientid=3
 clientsec="secret"
-ssoserver="https://example.com"
 console_api_scheme="https"
 
 # specify the client id, client secret and sso server here
 if [ "$environ" = "lain.local" ]; then
     clientid=3
     clientsec="secret_in_local"
-    ssoserver="https://example.com"
-	console_api_scheme="http"
+    redirecturi="https://example.com"
+    console_api_scheme="http"
 fi
 
 export SSO_CLIENT_ID=$clientid
 export SSO_CLIENT_SECRET=$clientsec
-export SSO_SERVER_NAME=$ssoserver
+export SSO_REDIRECT_URI=$redirecturi
 export CONSOLE_API_SCHEME=$console_api_scheme
 export CONSOLE_LOG_LEVEL="INFO"
 export CONSOLE_APISERVER="http://deployd.lain:9003"
