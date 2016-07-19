7c15b4ddcc2827895b4bb87ba04fc84d7269f2ad
kaizhang <kaizhang.shanxi@gmail.com>
2016-07-19 18:20:35 +0800

    fix quotes position

--- apis/specs.py
+++ apis/specs.py
@@ -336,9 +336,9 @@ def render_container_spec(app_name, proc):
     c.DnsSearch = [] if not hasattr(proc, 'dns_search') else copy.deepcopy(proc.dns_search)
     c.Volumes = copy.deepcopy(proc.volumes)
     c.SystemVolumes = copy.deepcopy(proc.system_volumes) + get_system_volumes_from_etcd(app_name)
-    logger.info("kai >>> proc.cmd: {}.format(proc.cmd)")
+    logger.info("kai >>> proc.cmd: {}".format(proc.cmd))
     c.Command = proc.cmd.split() # TODO solve the problem like `proc.cmd == ./run -p "a b c"`
-    logger.info("kai >>> c.Command: {}.format(c.Command)")
+    logger.info("kai >>> c.Command: {}".format(c.Command))
     c.Entrypoint = [] # TODO maybe force define to /lain/entrypoint/lain-entrypoint
     c.CpuLimit = proc.cpu
     c.MemoryLimit = humanfriendly.parse_size(proc.memory)
