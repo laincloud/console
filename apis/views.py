bc27e7e716e9b429c96f70349c098729a1b03be9
lipan <panli7@creditease.cn>
2016-08-08 12:34:10 +0800

    add lasterror field for proc

--- apis/views.py
+++ apis/views.py
@@ -648,7 +648,7 @@ class ProcApi:
             'dnssearchs': proc_lain_conf.dns_search,
             'ports': [{'portnumber': p.port, 'porttype': p.type.name} for p in proc_lain_conf.port.values()],
             'mountpoints': proc_lain_conf.mountpoint,
-            'https_only': proc_lain_conf.https_only,
+            'httpsonly': proc_lain_conf.https_only,
             'user': proc_lain_conf.user,
             'workingdir': proc_lain_conf.working_dir,
             'entrypoint': proc_lain_conf.entrypoint,
@@ -658,15 +658,14 @@ class ProcApi:
             'depends': [],
             'url': reverse('api_proc', kwargs={'appname': appname, 'procname': proc_lain_conf.name}),
             'logs': proc_lain_conf.logs,
+            'lasterror': '',
         }
         if proc_status:
-            pods = []
-            depends = []
+            pods, depends = [], []
+            last_error = ''
             try:
                 pods_meta = proc_status['Status']['Pods']
             except:
-                pods_meta = [] if not is_portal else {}
-            if pods_meta is None:
                 pods_meta = [] if not is_portal else {}
             # handle the situation when proc is portal
             if is_portal:
@@ -675,11 +674,14 @@ class ProcApi:
                         continue
                     for pod in pods_info:
                         pods.append(ProcApi.render_pod_data(pod))
+                        last_error = pod['LastError']
             else:
                 for pod in pods_meta:
                     pods.append(ProcApi.render_pod_data(pod))
+                last_error = proc_status['Status']['LastError']
             data['pods'] = pods
             data['depends'] = depends
+            data['lasterror'] = last_error
             # patch num_instances / cpu / memory spec in deploy to LainConf
             try:
                 data['numinstances'] = proc_status['Status']['Spec']['NumInstances']
