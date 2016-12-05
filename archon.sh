82fc6660510f39c96f3a7bf16c4d5dfadd2d534c
Jian Bai (Eric Pai) <ericpai94@hotmail.com>
2016-12-05 18:10:08 +0800

    Add entryServer config

--- archon.sh
+++ archon.sh
@@ -7,4 +7,4 @@ source ./config
 
 cd /lain/app/archon-0.1/
 
-exec ./archon-0.1.linux.amd64 -sso-client-id=$client_id -api-server="http://console."$environ -sso-server="http://sso."$environ -mode=frontend
+exec ./archon-0.1.linux.amd64 -sso-client-id=$client_id -api-server="http://console."$environ -sso-server="http://sso."$environ -entry-server="wss://entry."$environ -mode=frontend
