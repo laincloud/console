8f7011da0c30c01a0a5a26534a802ef8eb1332a3
chaoyiwang <chaoyiwang@creditease.cn>
2017-07-05 15:15:24 +0800

    can set console_api_server="/" in config as empty

--- archon.sh
+++ archon.sh
@@ -18,4 +18,12 @@ sso_server=${sso_server:-"http://sso."$environ}
 
 cd /lain/app/archon-0.1/
 
-exec ./archon-0.1.linux.amd64 -sso-client-id=$client_id -api-server=$console_api_scheme"://console."$environ -sso-server=$sso_server -entry-server=$ws_scheme"://entry."$environ -mode=frontend
+default_server=$console_api_scheme"://console."$environ
+api_server=${console_api_server:-$default_server}
+# for we can not set empty "" in the previous line, set console_api_server="/" in config instead
+if [ "$api_server" == "/" ];
+	then api_server=""
+fi
+
+
+exec ./archon-0.1.linux.amd64 -sso-client-id=$client_id -api-server=$api_server -sso-server=$sso_server -entry-server=$ws_scheme"://entry."$environ -mode=frontend
